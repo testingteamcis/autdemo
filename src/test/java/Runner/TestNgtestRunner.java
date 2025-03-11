a1f433e30e5e9659468d9845e71910507b198365
testingteamcis <pavanvetcha558@gmail.com>
2025-03-11 20:52:26 +0530

    this is file to update

--- src/test/java/Runner/TestNgtestRunner.java
+++ src/test/java/Runner/TestNgtestRunner.java
@@ -1,21 +1,40 @@
 package Runner;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Properties;
 
 import org.testng.annotations.AfterSuite;
-
-	import org.testng.annotations.DataProvider;
-
-	import io.cucumber.testng.AbstractTestNGCucumberTests;
-		import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import junit.textui.TestRunner;
 		
 		@CucumberOptions(features="src/test/java/feature/placevalidations.feature",glue="StepDefinitions",tags="@Addplace",monochrome=true,plugin= {"html:target/cucumber.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 
 		
 
 		public class TestNgtestRunner extends AbstractTestNGCucumberTests  {
+			
+			static {
+				try(InputStream input = TestRunner.class.getClassLoader().getResourceAsStream("extent.properties")){
+					
+					if(input==null) {
+						
+						System.err.println("Error: extent.properties is not found !");
+						
+					}else {
+						Properties prop = new Properties();
+						prop.load(input);
+						System.out.println("DEBUG: extent.properties loaded succeessfully!");
+					}
+					
+				} catch (IOException e) {
+					System.err.println("Error:Failed to load extent.properties -" +e.getMessage());
+				}
+			}
 
 		//	public static void main(String[] args) {
 			@Override
