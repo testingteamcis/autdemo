d9919f4e822e16d1c9af38905ae89fb36d42be29
testingteamcis <82110173+testingteamcis@users.noreply.github.com>
2025-03-17 14:48:50 +0530

    Update TestNgtestRunner.java

--- src/test/java/Runner/TestNgtestRunner.java
+++ src/test/java/Runner/TestNgtestRunner.java
@@ -39,21 +39,7 @@ import junit.textui.TestRunner;
 				}
 			}
 			
-			@BeforeClass
-			public static void setup() {
-				
-				String buildNumber= System.getProperty("BUILD_NUMBER","local");
-				
-				System.out.println("Cucumber Test Runner  Build Number: "+buildNumber);
-				String reportpath="test-output/SparkReport/Spark"+buildNumber+".html";
-				
-				System.setProperty("extent.reporter.spark.out", reportpath);
-				System.out.println("Extent report path:"+System.getProperty("extent.reporter.spark.out"));
-
-				
-
-			}
-
+			
 		//	public static void main(String[] args) {
 			@Override
 			@DataProvider(parallel=false) 
